aec6642e1ba22e036328d92a90a237b02942bedd
Kevin Young <kevinyounglogin@163.com>
2014-05-29 10:48:45 +0800

    clean the unneccessaries

--- coevolution/show.py
+++ coevolution/show.py
@@ -7,22 +7,10 @@ filename = "./data/prisoner.out"
 data = np.loadtxt(filename)
 
 t = data
-#f1 = data[:, 1]
-#f2 = data[:, 2]
 
-#t1 = data1[:, 0]
-#f3 = data1[:, 1]
-
-#with open("npsave.txt", "w") as f:
-    #np.savetxt(f, data)
 
 fig, ax = plt.subplots()
 ax.plot(t)
 
-#fig1, ax1 = plt.subplots()
-#ax1.plot(t1, f3/3200.)
-#ax.plot(f2)
-#ax.set_ylim([0.14, 0.26])
 ax.set_xlim([0, 4000])
-#ax.plot(f3)
 plt.show()
