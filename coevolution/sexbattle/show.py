e1e87f7d1473af066d35a0e94e430f45ab5a0197
Kevin Young <kevinyounglogin@163.com>
2014-06-04 14:21:44 +0800

    add label

--- coevolution/sexbattle/show.py
+++ coevolution/sexbattle/show.py
@@ -17,6 +17,8 @@ fig, ax = plt.subplots()
 
 ax.set_xlim([-10, 510])
 ax.set_ylim([-0.002, 0.005])
+ax.set_xlabel("N")
+ax.set_ylabel("$<\Delta H>N$")
 
 scatter_moran = ax.scatter(n, moran, color='blue')
 scatter_local = ax.scatter(n, local, color='red')
